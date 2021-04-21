118aeba0763a251ddc00a45a61eb42c50735a942
Mandell Butler <mandell.butler@outlook.com>
2021-04-21 18:25:59 -0400

    Added more detail to psuedo code

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,47 @@
 //=====================DEPENDENCIES
 
+//current day
+
+//hour of the day
+
+//save button
+
+//ability to scroll
+
+//input area
+
+//local storage
+
+//color changing based on time of the day
 
 //=====================STARTING DATA
 
 //CONTAINER
 
+//color coded time blocks
+
+//hours presented in 9-5 format
+
 //====================USER INTERACTIONS
 
 // WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
+    //I am presented with a calendar
+    //the current day is displayed at the top
+    //the planner displays a schedule of events
+
+
 // WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+    //I am presented with timeblocks for standard business hours
+    //timeblocks are color coded for past, present, or future
+
+
 // WHEN I click into a timeblock
-// THEN I can enter an event
+    //I can enter an event
+
+    
 // WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
+    //the text for that event is saved in local storage
+
 
 //=====================INITIALIZATION
 
